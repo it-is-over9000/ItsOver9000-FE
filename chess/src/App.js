f47992b7f19546cfacd89b4d2650ac6594bb5992
Josh Akeman <joshakeman@gmail.com>
2019-04-12 22:45:01 -0400

    got react app set up, knight is rendering

---
+++ chess/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Knight from './components/Knight'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Knight />
+      </div>
+    );
+  }
+}
+
+export default App;
