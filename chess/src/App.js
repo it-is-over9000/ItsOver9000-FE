4a2bac67e46f68e50d895d8cb978d69cfca8900b
Josh Akeman <joshakeman@gmail.com>
2019-04-16 08:24:19 -0400

    board rendering

--- chess/src/App.js
+++ chess/src/App.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import ReactDOM from 'react-dom'
 
-import Knight from './components/Knight'
-import Square from './components/Square'
-import Board from './components/Board'
+import WithMoveValidation from "./components/WithMoveValidation";
 import Login from './components/Login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <Board knightPosition={[3,2]} /> */}
-        <Login />
+      <div>
+        <div style={boardsContainer}>
+          <WithMoveValidation />
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+const boardsContainer = {
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center",
+  flexWrap: "wrap",
+  width: "100vw",
+  marginTop: 30,
+  marginBottom: 50
+};
--- chess/src/index.js
+++ chess/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import Board from './components/Board'
+// import Board from './components/Board'
 
 ReactDOM.render(
     <App />, 
